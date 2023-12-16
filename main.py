30de68874656ddab6d2ec9052a66a96954bf23d0
yaznzamel <67708266+yazanzamel@users.noreply.github.com>
2023-12-16 05:25:09 -0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# backend/main.py
-from fastapi import FastAPI
-import uvicorn
-
-app = FastAPI()
-
-@app.get("/get-text")
-async def get_text():
-    return {"message": "Hello from FastAPI backend!"}
